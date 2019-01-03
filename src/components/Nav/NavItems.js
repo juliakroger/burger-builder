e9715700ccd5ea18ae24f3665e2f3a57ffad85a0
juliakroger <julia.kroger@gmail.com>
2019-01-03 13:44:06 -0200

    files update

--- src/components/Nav/NavItems.js
+++ src/components/Nav/NavItems.js
@@ -4,7 +4,8 @@ import classes from './Style.css';
 const navItems = (props) => (
     <ul className={classes.NavItems}>
         <li className={classes.Item}><a href="/">Burger Builder</a></li>
-        <li className={classes.Item}><a href="/">Checkout</a></li>
+        <li className={classes.Item}><a href="/">Past Orders</a></li>
+        <li className={classes.Item}><a href="/">Log out</a></li>
     </ul>
 );
 
